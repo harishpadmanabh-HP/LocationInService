c87270988a09a84cd094e27dc08438b62fe02b67
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2019-07-28 00:50:51 +0530

    location update with service

---
+++ app/src/main/java/com/hp/locationinservice/LocalService.java
@@ -0,0 +1,167 @@
+package com.hp.locationinservice;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class LocalService extends Service implements LocationListener
+{
+    boolean isGPSEnable = false;
+    boolean isNetworkEnable = false;
+    double latitude, longitude;
+    LocationManager locationManager;
+    Location location;
+    private Handler mHandler = new Handler();
+    private Timer mTimer = null;
+    long notify_interval = 1000;
+    public static String str_receiver = "servicetutorial.service.receiver";
+    Intent intent;
+    private static Timer timer = new Timer();
+    private Context ctx;
+
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+    public void onCreate()
+    {
+        super.onCreate();
+        ctx = this;
+        startService();
+    }
+
+    private void startService()
+    {
+        timer.scheduleAtFixedRate(new mainTask(), 0, 5000);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getApplicationContext(), "loc update test service"+location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    private class mainTask extends TimerTask
+    {
+        public void run()
+        {
+            toastHandler.sendEmptyMessage(0);
+        }
+    }
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Toast.makeText(this, "Service Stopped ...", Toast.LENGTH_SHORT).show();
+    }
+
+    private final Handler toastHandler = new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg)
+        {
+            Toast.makeText(getApplicationContext(), "test service", Toast.LENGTH_SHORT).show();
+
+            fn_getlocation();
+        }
+    };
+    private void fn_getlocation() {
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        isGPSEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnable && !isNetworkEnable) {
+
+        } else {
+
+            if (isNetworkEnable) {
+                location = null;
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    Activity#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, this);
+                if (locationManager!=null){
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (location!=null){
+
+                        Log.e("latitude",location.getLatitude()+"");
+                        Log.e("longitude",location.getLongitude()+"");
+
+                        Toast.makeText(getApplicationContext(), "loc update test service"+location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        fn_update(location);
+                    }
+                }
+
+            }
+
+
+            if (isGPSEnable){
+                location = null;
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,0,this);
+                if (locationManager!=null){
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location!=null){
+                        Log.e("latitude",location.getLatitude()+"");
+                        Log.e("longitude",location.getLongitude()+"");
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+                        Toast.makeText(getApplicationContext(), "gps false last loc update test service"+location.getLatitude()+" "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+                        fn_update(location);
+                    }
+                }
+            }
+
+
+        }
+
+    }
+    private void fn_update(Location location){
+        intent = new Intent(str_receiver);
+ 
+        intent.putExtra("latutide",location.getLatitude()+"");
+        intent.putExtra("longitude",location.getLongitude()+"");
+        sendBroadcast(intent);
+    }
+
+}
